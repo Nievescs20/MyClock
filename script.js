ef79fcacab2321848e202b3e809fe04ce0aa4fc7
jonblanco-gif <76707900+jonblanco-gif@users.noreply.github.com>
2021-06-17 12:29:55 -0400

    broken rip

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Your code here
+const table = document.getElementsByTagName('table')
+
+function makeRow () {
+  const tr = document.createElement('tr')
+  table[0].appendChild(tr)
+  for (let i = 0; i < 20; i++) {
+    const td = document.createElement('td')
+    tr.appendChild(td)
+  }
+}
+
+const button = document.getElementById('add-row')
+
+button.addEventListener('click', makeRow)
+
