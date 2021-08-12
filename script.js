92dae84e660fc33ab76b11d3eb73ab4c4400b1e7
ChrisNieves <nievescs20@gmail.com>
2021-08-12 12:52:36 -0400

    minor change to clock logic

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 // Your code here
 
 function showTime() {
-  var date = new Date();
-  var h = date.getHours();
-  var m = date.getMinutes();
-  var s = date.getSeconds();
-  var session = 'AM';
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let session = "PM";
+  console.log(date);
 
   if (h == 0) {
     h = 12;
   }
 
   if (h > 12) {
+    session = "AM";
     h = h - 12;
-    session = 'PM';
   }
 
-  h = h < 10 ? '0' + h : h;
-  m = m < 10 ? '0' + m : m;
-  s = s < 10 ? '0' + s : s;
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
 
-  var time = h + ':' + m + ':' + s + ' ' + session;
-  document.getElementById('MyClockDisplay').innerText = time;
-  document.getElementById('MyClockDisplay').textContent = time;
+  let time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
 
   setTimeout(showTime, 1000);
 }
 
 function registerSW() {
-  if ('serviceWorker' in navigator) {
+  if ("serviceWorker" in navigator) {
     showTime();
     navigator.serviceWorker
-      .register('./sw.js')
-      .then((reg) => console.log('Service Worker: Registered'))
+      .register("./sw.js")
+      .then((reg) => console.log("Service Worker: Registered"))
       .catch((err) => console.log(`Service Worker: Error ${err}`));
   }
 }
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   registerSW();
 });
